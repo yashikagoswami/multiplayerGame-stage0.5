35696d0aac045ddb14ba015f0305047fd9043a73
YashikaGoswami26 <71645264+yashikagoswami@users.noreply.github.com>
2021-01-06 16:12:33 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,55 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+async  start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef= await database.ref('playerCount').once("value")
+      if(playerCountRef.exists()) {
+        playerCountRef=playerCountRef.val()
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+  }
+  play() {
+    form.hide();
+    text("game start",120,100);
+    textSize(30);
+    Player.getPlayerInfo();
+
+    if(allPlayers!==undefined)
+    {
+      var display_position=130;
+      for(
+        var plr in allPlayers
+      ){
+        if(plr==="player"+player.index)
+        fill("red")
+        else(fill("black"))
+        display_position+=20
+        textSize(20)
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,display_position)
+      }
+    }
+    if(keyIsDown(UP_ARROW) && player.index!==null) {
+       player.distance+=50
+       player.update();
+    }
+  }
+}
